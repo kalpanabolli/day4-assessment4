7cdad3a15d645943d2d1c8358d2b57ce24448146
kalpanabolli11@gmail.com <kalpanabolli11@gmail.com>
2023-07-14 18:32:47 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day4_exercise4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("Enter number");
+            num = int.Parse(Console.ReadLine());
+            if (num != 0)
+            {
+                var result = num % 2 == 0 ? "The Number is Even" : "The Number is Odd";
+
+                if (num != 0)
+                {
+
+                    var result1 = num > 0 ? "The Number is Positive" : "The Number is Negative";
+                    Console.WriteLine(result1);
+                }
+                Console.WriteLine(result);
+            }
+            else if (num == 0)
+            {
+                Console.WriteLine("number is Zero");
+            }
+            Console.ReadKey();
+        }
+    }
+}
